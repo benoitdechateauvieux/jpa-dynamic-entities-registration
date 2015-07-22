9b62b313e43ae28652dcf8f3459f0de776cc9a2f
Benoit de Chateauvieux <bdechateauvieux@exoplatform.com>
2015-07-22 18:21:01 +0700

    replace Scannotation with APT

---
+++ commons/src/main/java/org/exoplatform/bch/jpadynamicentitiesregistration/ExoEntityProcessor.java
@@ -0,0 +1,50 @@
+package org.exoplatform.bch.jpadynamicentitiesregistration;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.FilerException;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.tools.FileObject;
+import javax.tools.StandardLocation;
+
+/**
+ * Created by The eXo Platform SAS
+ * Author : eXoPlatform
+ * exo@exoplatform.com
+ * 7/22/15
+ */
+@SupportedAnnotationTypes("org.exoplatform.bch.jpadynamicentitiesregistration.ExoJpaEntity")
+
+public class ExoEntityProcessor extends AbstractProcessor {
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        Writer writer = null;
+        try {
+            FileObject file = processingEnv.getFiler().createResource(StandardLocation.CLASS_OUTPUT, "", "exo/exo-entities.txt");
+            writer = file.openWriter();
+            for (Element element : roundEnv.getElementsAnnotatedWith(ExoJpaEntity.class)) {
+                writer.append(element.asType().toString());
+                System.out.println(element.asType());
+            }
+        } catch (FilerException e) {
+            //Second round: the file as already been created
+        } catch (IOException e) {
+            e.printStackTrace(); //TODO
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace(); //TODO
+                }
+            }
+        }
+        return true; //No need to process these annotations again
+    }
+}
--- commons/src/main/java/org/exoplatform/bch/jpadynamicentitiesregistration/ExoJpaEntityScanner.java
+++ commons/src/main/java/org/exoplatform/bch/jpadynamicentitiesregistration/ExoJpaEntityScanner.java
@@ -1,45 +1,63 @@
 package org.exoplatform.bch.jpadynamicentitiesregistration;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Enumeration;
+import java.util.Properties;
 
 import org.hibernate.cfg.Configuration;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.integrator.spi.Integrator;
 import org.hibernate.metamodel.source.MetadataImplementor;
 import org.hibernate.service.spi.SessionFactoryServiceRegistry;
-import org.scannotation.AnnotationDB;
-import org.scannotation.ClasspathUrlFinder;
 
 public class ExoJpaEntityScanner implements Integrator {
     public void integrate(Configuration configuration,
                           SessionFactoryImplementor sessionFactory,
                           SessionFactoryServiceRegistry serviceRegistry) {
-        Set<Class> plfEntities = new HashSet<>();
 
-        long start3 = System.currentTimeMillis();
-        URL[] urls = ClasspathUrlFinder.findClassPaths(); // scan java.class.path
-        AnnotationDB db = new AnnotationDB();
+        registerHibernateConfiguration(configuration);
+        registerEntities(configuration);
+    }
+
+    private void registerHibernateConfiguration(Configuration configuration) {
+        final Properties properties = new Properties();
         try {
-            db.scanArchives(urls);
+            properties.load(this.getClass().getResourceAsStream("/exo.properties"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        for (String entityClass : db.getAnnotationIndex().get(ExoJpaEntity.class.getName())) {
-            try {
-                plfEntities.add(Class.forName(entityClass));
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
+        String propertyValue = properties.getProperty("hibernate.show_sql");
+        if (propertyValue!=null) {
+            Properties props = new Properties();
+            props.put("hibernate.show_sql", propertyValue);
+            configuration.addProperties(props);
+            System.out.println("Setting hibernate.show_sql to [" + propertyValue+"]");
         }
-        long end3 = System.currentTimeMillis();
-        System.out.println("Scannotation : " +(end3-start3)+ " ms");
+    }
 
-
-        for (Class entityClass : plfEntities) {
-            configuration.addAnnotatedClass(entityClass);
+    private void registerEntities(Configuration configuration) {
+        try {
+            Enumeration<URL> urls = ClassLoader.getSystemClassLoader().getResources("exo/exo-entities.txt");
+            while (urls.hasMoreElements()) {
+                InputStream stream = urls.nextElement().openStream();
+                try {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+                    String entityClassName;
+                    while ((entityClassName = reader.readLine()) != null) { //TODO isBlank
+                        configuration.addAnnotatedClass(Class.forName(entityClassName));
+                    }
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                } finally {
+                    try { stream.close(); } catch (Throwable ignore) {}
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         configuration.buildMappings();
     }
